2114de32ac1970873c30589aee82eab0cc8c3b4c
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-07-21 13:13:08 +0200

    adding post skills request

--- routes/skills.js
+++ routes/skills.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const connection = require('../db-config');
+const joi = require('joi');
 
 router.get('/', (req, res) => {
   const sql = 'SELECT * FROM skill';
@@ -10,6 +11,30 @@ router.get('/', (req, res) => {
     .catch((err) => {
       res.status(500).json({ message: `Error while retrieving skills : ${err.message}` });
     });
+});
+
+router.post('/', (req, res) => {
+    const { label, level } = req.body;
+
+    const { error: validationErrors } = joi.object({
+        label: joi.string().max(80).required(),
+        level: joi.string().max(50),
+    }).validate( { label, level }, { abortEarly: false });
+
+    if(validationErrors) {
+        res.status(422).json({ errors: validationErrors.details })
+    } else {
+
+        const sql = 'INSERT INTO skill (label, level) VALUES (?, ?)';
+
+        connection.promise().query(sql, [label, level])
+                    .then(([{insertId}]) => {
+                        res.status(201).json({ insertId, label, level });
+                    })
+                    .catch((err) => {
+                        res.status(500).send(`Error server: ${err.message}`);
+                    });
+    }
 });
 
 module.exports = router;
