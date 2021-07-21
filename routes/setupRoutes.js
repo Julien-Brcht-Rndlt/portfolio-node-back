45f38cebd6fb6b7b956d2b0cc501a5e826486ef5
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-07-21 12:51:42 +0200

    adding get skills route

--- routes/setupRoutes.js
+++ routes/setupRoutes.js
@@ -1,9 +1,11 @@
 const myselfRouter = require('./myself');
 const projectsRouter = require('./myself');
+const skillsRouter = require('./myself');
 
 const setupRoutes = (app) => {
   app.use('/myself', myselfRouter);
   app.use('/projects', projectsRouter);
+  app.use('/skills', skillsRouter);
 };
   
 module.exports = {
---
+++ routes/skills.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+const connection = require('../db-config');
+
+router.get('/', (req, res) => {
+  const sql = 'SELECT * FROM skills';
+  connection
+    .promise()
+    .query(sql)
+    .then(([results]) => res.status(200).json(results))
+    .catch((err) => {
+      res.status(500).json({ message: `Error while retrieving skills : ${err.message}` });
+    });
+});
+
+module.exports = router;
