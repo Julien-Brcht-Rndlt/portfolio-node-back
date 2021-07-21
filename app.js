3332993cb242860355e01dc597d6e720234c6568
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-07-21 10:55:10 +0200

    basic config app + index

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const connection = require('./db-config');
+const { setupRoutes } = require('./routes/setupRoutes');
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+setupRoutes(app);
+
+connection.connect((err) => {
+  if (err) {
+    console.error(`error connecting: ${err.stack}`);
+  } else {
+    console.log(`connected as id ' ${connection.threadId}`);
+  }
+});
+
+module.exports = app;
