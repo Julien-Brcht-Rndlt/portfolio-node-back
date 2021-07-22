58e210072d04f11c310eba4287d6339d41f2bd44
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-07-22 12:43:25 +0200

    adding new routes to settings

--- routes/setupRoutes.js
+++ routes/setupRoutes.js
@@ -1,11 +1,15 @@
 const myselfRouter = require('./myself');
 const projectsRouter = require('./projects');
 const skillsRouter = require('./skills');
+const loginRouter = require('./login');
+const registerRouter = require('./register');
 
 const setupRoutes = (app) => {
   app.use('/myself', myselfRouter);
   app.use('/projects', projectsRouter);
   app.use('/skills', skillsRouter);
+  app.use("/auth/login", loginRouter);
+  app.use("/auth/register", registerRouter);
 };
   
 module.exports = {
